684a3257d4e76b3ab4ce8fcd8f78d0752f5b34dd
Sakshamvnm <149808233+Sakshamvnm@users.noreply.github.com>
2024-02-07 22:52:13 +0545

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,51 @@
+const chatBody = document.querySelector(".chat-body");/*from index.html*/
+const textinput = document.querySelector("#textinput");
+const send = document.querySelector(".send");
+
+send.addEventListener("click", () => {
+	console.log("Send button clicked");
+	renderUserMessage()}
+	);
+
+textinput.addEventListener("keyup", (event) =>{
+	console.log("Key pressed:",event.keyCode);
+	if (event.keyCode === 13)/*13 corresponds to the enter key so can enter without clicking enter*/
+	{
+		renderUserMessage();
+	}
+});
+
+const renderUserMessage = () => {
+	const userInput = textinput.value;
+	console.log("userInput:",userInput);
+	renderMessageELe(userInput);
+	textinput.value ="";  /*pressing enter displays message*/
+	renderChatbotResponse(userInput);
+};
+
+const renderChatbotResponse = (userInput) => {
+	const res = getChatbotResponse(userInput);
+	console.log("Chatbot response:",res);
+	renderMessageELe(res);
+}
+
+const renderMessageELe= (txt) => {
+	const messageELe = document.createElement("div");
+	const txtNode = document.createTextNode(txt);
+	messageELe.classList.add("user-message");
+	messageELe.appendChild(txtNode);
+	chatBody.appendChild(messageELe);
+};
+const responseObj = {
+	hello:"Hey ! How are you doing?",
+	Hey:"Namaste ! how are you doing?",
+	Bye:"Bye sir.Have a nice day!",
+	today: new Date().toDateString(),
+	time: new Date().toLocaleTimeString(),
+};
+
+ const getChatbotResponse = (userInput) => {
+ 	 
+
+	return responseObj[userInput] ==undefined?"Sorry I can't understand.": responseObj[userInput];/*If the user input is not availabe in object keys it is gonna return the undefined values given in response.js*/
+};
